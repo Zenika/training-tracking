5bae4de05aa21dba727f718ac220339e790f0fb3
Matthias FERRAINA <mferraina.prestataire@si2m.fr>
2021-09-30 13:15:27 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # training-tracking
+## Description
+A script to retrieve current training progression depending on available time, and training support number of pages.
+
+## Install
+Clone the repo and just ```npm start``` to see the beautiful training progression status
+
+## Todo
+use env variable to input directly current support page
---
+++ index.js
@@ -0,0 +1,60 @@
+const currentTime = new Date();
+const startTime = new Date("2021-09-30 09:30:00 GMT+2:00");
+const mealDuration = 1 * 3600 * 1000;
+const mealTime = new Date("2021-09-30 12:30:00 GMT+2:00");
+
+const elapsedTime =
+  mealTime - currentTime >= 0
+    ? currentTime - startTime
+    : currentTime - startTime - mealDuration;
+
+const availableTime = 7 * 3600 * 1000;
+const pdfTotalPages = 272;
+
+// A modifier pour mettre à jour la progression
+const pdfCurrentPage = 118;
+
+const pdfExpectedPage = Math.floor(
+  (elapsedTime / availableTime) * pdfTotalPages
+);
+
+const formatMessage = (
+  trainingStatus,
+  pdfCurrentPage,
+  pdfTotalPages,
+  pdfExpectedPage
+) => {
+  const progressionPourcentage = Math.ceil(
+    (pdfCurrentPage / pdfTotalPages) * 100
+  );
+  const progressionRelative = Math.abs(pdfCurrentPage - pdfExpectedPage);
+  return `${progressionRelative} pages en ${trainingStatus} - page ${pdfCurrentPage}/${pdfTotalPages}(${progressionPourcentage}% progression) - on devrait être à la page ${pdfExpectedPage}/${pdfTotalPages}`;
+};
+
+const assertTrainingIsOnTime = (pdfExpectedPage, pdfCurrentPage) => {
+  return pdfExpectedPage > pdfCurrentPage;
+};
+
+const isTrainingOnTime = assertTrainingIsOnTime(
+  pdfExpectedPage,
+  pdfCurrentPage
+);
+let message;
+
+if (isTrainingOnTime) {
+  message = formatMessage(
+    "RETARD 🐌!",
+    pdfCurrentPage,
+    pdfTotalPages,
+    pdfExpectedPage
+  );
+} else {
+  message = formatMessage(
+    "AVANCE 🐇!",
+    pdfCurrentPage,
+    pdfTotalPages,
+    pdfExpectedPage
+  );
+}
+
+console.log(message);
